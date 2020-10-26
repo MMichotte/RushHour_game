e9543e18bb8ec03ae3e14581f1c27f94e0b8a1d6
MMichotte <martin.michotte@gmail.com>
2020-10-26 16:35:28 +0100

    finished collisions

--- src/controller/blockController.py
+++ src/controller/blockController.py
@@ -25,50 +25,92 @@ class BlockController:
             authorizedMovements = self.checkColision()
             dx = event.x - self.currentX 
             dy = event.y - self.currentY
-            if self.block.orientation == "H":  
-                if dx < 0 and "R" not in authorizedMovements:
-                    pass
-                elif dx > 0 and "L" not in authorizedMovements:
-                    pass
-                else:
-                    self.parent.move(self.blockView, dx, 0)
+            if self.block.orientation == "H":
+                if "X" in authorizedMovements:
+                    if dx > 0 and "R" not in authorizedMovements:
+                        pass
+                    elif dx < 0 and "L" not in authorizedMovements:
+                        pass
+                    else:
+                        self.parent.move(self.blockView, dx, 0)
+                else:  
+                    if dx < 0 and "R" not in authorizedMovements:
+                        pass
+                    elif dx > 0 and "L" not in authorizedMovements:
+                        pass
+                    else:
+                        self.parent.move(self.blockView, dx, 0)
             else:
-                if dy < 0 and "T" not in authorizedMovements:
-                    pass
-                elif dy > 0 and "B" not in authorizedMovements:
-                    pass
+                if "X" in authorizedMovements:
+                    if dy > 0 and "T" not in authorizedMovements:
+                        pass
+                    elif dy < 0 and "B" not in authorizedMovements:
+                        pass
+                    else:
+                        self.parent.move(self.blockView, 0, dy)
                 else:
-                    self.parent.move(self.blockView, 0, dy)
+                    if dy < 0 and "T" not in authorizedMovements:
+                        pass
+                    elif dy > 0 and "B" not in authorizedMovements:
+                        pass
+                    else:
+                        self.parent.move(self.blockView, 0, dy)
             self.currentX = event.x
             self.currentY = event.y
 
     def checkColision(self):
+        padding = 5
+        border = self.parent.coords(self.parent.find_withtag("border")[0])
+        rightTopBorder = [border[0]-padding,border[1]-padding,border[2]-padding,border[3]+padding]
+        topBorder = [border[2]-padding,border[3]+padding,border[4]+padding,border[5]+padding]
+        leftBorder = [border[4]+padding,border[5]+padding,border[6]+padding,border[7]-padding]
+        bottomBorder = [border[6]+padding,border[7]-padding,border[8]-padding,border[9]-padding]
+        rightBottomBorder = [border[8]-padding,border[9]-padding,border[10]-padding,border[11]+padding]
+
         authorizedMovements = ""
         thisCoords = self.parent.bbox(self.blockView)
         if self.block.orientation == "H":
             thisCoordsA = [int(thisCoords[0]),int(thisCoords[1]+(thisCoords[3]-thisCoords[1])/2)] #milieu gauche
             thisCoordsB = [int(thisCoords[2]),int(thisCoords[1]+(thisCoords[3]-thisCoords[1])/2)] #milieu droite
             authorizedMovements = "LR"
+
+            if thisCoordsA[0] < leftBorder[0]:
+                    authorizedMovements = "RX"
+            if thisCoordsA[1] in range(int(rightTopBorder[3]),int(rightTopBorder[1])):
+                if thisCoordsB[0] > rightTopBorder[0]:
+                    authorizedMovements = "LX"
+            if thisCoordsA[1] in range(int(rightBottomBorder[3]),int(rightBottomBorder[1])):
+                if thisCoordsB[0] > rightBottomBorder[0]:
+                    authorizedMovements = "LX"
+            
             for b in self.block.getBlocks():
                 if self.block!= b:
                     otherCoords = self.parent.bbox(b.view)
                     if thisCoordsA[1] in range(int(otherCoords[1]),int(otherCoords[3])):
-                        if thisCoordsA[0] in range(int(otherCoords[0]),int(otherCoords[2])):
-                            authorizedMovements = "L"
-                        if thisCoordsB[0] in range(int(otherCoords[0]),int(otherCoords[2])):
-                            authorizedMovements = "R"
+                        if thisCoordsA[0] in range(int(otherCoords[0])-padding,int(otherCoords[2])+padding):
+                            if authorizedMovements != "R":
+                                authorizedMovements = "L"
+                        if thisCoordsB[0] in range(int(otherCoords[0])-padding,int(otherCoords[2])+padding):
+                            if authorizedMovements != "L":
+                                authorizedMovements = "R"
 
         else:
             thisCoordsA = [int(thisCoords[0] + (thisCoords[2]-thisCoords[0])/2), int(thisCoords[1])] #milieu haut
             thisCoordsB = [int(thisCoords[0] + (thisCoords[2]-thisCoords[0])/2), int(thisCoords[3])] #milieu bas
             authorizedMovements = "TB"
+
+            if thisCoordsA[1] < topBorder[1]:
+                authorizedMovements = "TX"
+            if thisCoordsB[1] > bottomBorder[1]:
+                authorizedMovements = "BX"
+
             for b in self.block.getBlocks():
                 if self.block != b:
                     otherCoords = self.parent.bbox(b.view)
                     if int(thisCoordsA[0]) in range(int(otherCoords[0]),int(otherCoords[2])):
-                        if thisCoordsA[1] in range(int(otherCoords[1]),int(otherCoords[3])):
+                        if thisCoordsA[1] in range(int(otherCoords[1])-padding,int(otherCoords[3])+padding):
                             authorizedMovements = "B"
-                        if thisCoordsB[1] in range(int(otherCoords[1]),int(otherCoords[3])):
+                        if thisCoordsB[1] in range(int(otherCoords[1])-padding,int(otherCoords[3])+padding):
                             authorizedMovements = "T"
         
         return authorizedMovements
--- src/view/lvl1.py
+++ src/view/lvl1.py
@@ -23,14 +23,14 @@ class Lvl1:
             0+self.borderWidth,0+self.borderWidth,
             0+self.borderWidth,self.gameH,
             self.gameW,self.gameH,
-            self.gameW,self.gameH/2+self.blockWidth/2,width=self.borderWidth
+            self.gameW,self.gameH/2+self.blockWidth/2,width=self.borderWidth,tags="border"
         )
         
         for l in range(self.numberOfLines):
             self.canvas.create_line(0,self.borderWidth+l*self.blockWidth,self.gameW,self.borderWidth+l*self.blockWidth)
             self.canvas.create_line(self.borderWidth+l*self.blockWidth,0,self.borderWidth+l*self.blockWidth,self.gameW)
 
-        self.block_1 = Block(self.canvas,width=self.blockWidth,size=2,orientation="H",position=[3,3],color="red")
+        self.block_0 = Block(self.canvas,width=self.blockWidth,size=2,orientation="H",position=[3,3],color="red")
         self.block_2 = Block(self.canvas,width=self.blockWidth,size=3,orientation="V",position=[1,2],color="blue")
         self.block_3 = Block(self.canvas,width=self.blockWidth,size=3,orientation="V",position=[6,2],color="orange")
         self.block_4 = Block(self.canvas,width=self.blockWidth,size=3,orientation="H",position=[4,0],color="green")
