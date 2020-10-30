c6d35de1d90711179af211f5252ba3a0e82138f4
MMichotte <martin.michotte@gmail.com>
2020-10-30 08:32:25 +0100

    release

--- WIN_setup.py
+++ WIN_setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup
+from distutils.core import setup
+import py2exe, sys, os
 
 APP = ['src/main.py']
 DATA_FILES = []
@@ -13,3 +14,13 @@ setup(
     options={'py2exe': OPTIONS},
     setup_requires=['py2exe'],
 )
+
+"""
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    windows = [{'script': "single.py"}],
+    zipfile = None,
+)
+"""
