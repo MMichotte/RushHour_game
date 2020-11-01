c690ca8397b1c45d8c3ac6b4677e16b55d4df046
MMichotte <martin.michotte@gmail.com>
2020-11-01 17:04:38 +0100

    updated level order

--- src/view/levels/lvl4.py
+++ src/view/levels/lvl4.py
@@ -10,13 +10,15 @@ class Lvl4:
 
         self.blockBaseSettings = [self.canvas,self.blockWidth]
 
-        self.block_0 = Block(self.blockBaseSettings, size=2, orientation="H", position=[0, 2], color="red", isMain=True)
-        self.block_2 = Block(self.blockBaseSettings, size=3, orientation="H", position=[0, 0], color="black")
-        self.block_3 = Block(self.blockBaseSettings, size=2, orientation="H", position=[0, 4], color="orange")
-        self.block_4= Block(self.blockBaseSettings, size=3, orientation="V", position=[5, 1], color="gold")
-        self.block_5 = Block(self.blockBaseSettings, size=2, orientation="V", position=[3, 1], color="green")
-        self.block_6 = Block(self.blockBaseSettings, size=2, orientation="H", position=[3, 3], color="lightBlue")
-        self.block_7 = Block(self.blockBaseSettings, size=2, orientation="V", position=[2, 3], color="maroon1")
-        self.block_8 = Block(self.blockBaseSettings, size=3, orientation="H", position=[3, 5], color="purple")
-        self.block_8 = Block(self.blockBaseSettings, size=3, orientation="V", position=[4, 0], color="navy")
-
+        self.block_0 = Block(self.blockBaseSettings,size=2,orientation="H",position=[1,2],color="red", isMain=True)
+        self.block_2 = Block(self.blockBaseSettings,size=3,orientation="H",position=[3,0],color="maroon1")
+        self.block_3 = Block(self.blockBaseSettings,size=2,orientation="V",position=[2,0],color="green")
+        self.block_4 = Block(self.blockBaseSettings,size=2,orientation="V",position=[0,1],color="grey")
+        self.block_5 = Block(self.blockBaseSettings,size=2,orientation="H",position=[4,1],color="lightBlue")
+        self.block_6 = Block(self.blockBaseSettings,size=3,orientation="V",position=[3,1],color="purple")
+        self.block_7 = Block(self.blockBaseSettings,size=2,orientation="V",position=[1,3],color="maroon1")
+        self.block_8 = Block(self.blockBaseSettings,size=2,orientation="H",position=[4,3],color="Blue")
+        self.block_9 = Block(self.blockBaseSettings,size=2,orientation="V",position=[0,4],color="green")
+        self.block_10 = Block(self.blockBaseSettings,size=2,orientation="V",position=[5,4],color="gold")
+        self.block_11 = Block(self.blockBaseSettings,size=2,orientation="H",position=[2,4],color="black")
+        self.block_12 = Block(self.blockBaseSettings,size=3,orientation="H",position=[1,5],color="navy")
